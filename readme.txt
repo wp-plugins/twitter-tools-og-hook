33298d49dd06ee20157d4f91b727406dc67a1f43
shukitchan <shukitchan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-25 05:36:11 +0000

    First Version
    git-svn-id: https://plugins.svn.wordpress.org/twitter-tools-og-hook/trunk@428466 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Twitter Tools OG Hook ===
+Tags: twitter, widget, plugin, Facebook
+Contributors: shukitchan
+Requires at least: 2.9
+Tested up to: 3.0.1
+Stable tag: 0.1
+
+Twitter Tools OG Hook is a plugin that works with Twitter Tools and allows you to post update stream for your web page through the Facebook open graph protocol for the blog post you created. 
+
+== Details ==
+
+[Twitter Tools](http://wordpress.org/extend/plugins/twitter-tools/) is an excellent plugin for posting notifications of new blog posts to [Twitter](http://twitter.com/). This plugin allows you to extend that and post those notifications to the stream of your page as well through the Facebook open graph protocol.
+
+**Requirements**
+
+* [Twitter Tools](http://wordpress.org/extend/plugins/twitter-tools/) be installed and activated
+* PHP 5.2.0 or newer
+* WordPress 2.9 or newer
+
+== Installation ==
+
+1. Download the plugin archive and expand it (you've likely already done this).
+2. Put the 'twitter-tools-og-hook' directory into your wp-content/plugins/ directory.
+3. Go to the Plugins page in your WordPress Administration area and click 'Activate' for Twitter Tools - Open Graph Hook
+4. Follow the instructions [here](http://developers.facebook.com/docs/opengraph/) to turn your page into Facebook open graph objects. Make sure you associate your page with a Facebook application using the "fb:app_id" meta tag.
+5. Obtain the page url, app ID and app secret of your page. Go to the Twitter Tools Options page (Settings > Twitter Tools) to set up your Twitter Tools Open Graph Hook by filling those information.
+
+== Frequently Asked Questions ==
+
+= It's not working! =
+
+Did you make sure to fill in your page URL, facebook app ID and secret on Twitter Tools plugin's settings page on Open Graph Hook section?
+
+== Changelog ==
+
+= 0.1 =
+
+* First Release
+
---
+++ twitter_tools_og_hook.php
@@ -0,0 +1,232 @@
+<?php
+/*
+Plugin Name: Twitter Tools - Open Graph Hook
+Plugin URI:  
+Description: Allow for blog posts published to be sent to a Facebook Open Graph Node (e.g. a facebook page) This plugin relies on Twitter Tools, configure it on the Twitter Tools settings page.
+Version: 0.1
+Author: Shu Kit Chan
+Author URI: http://www.shukitchan.com
+*/
+
+// ini_set('display_errors', '1'); ini_set('error_reporting', E_ALL);
+
+if (!defined('PLUGINDIR')) {
+	define('PLUGINDIR','wp-content/plugins');
+}
+
+load_plugin_textdomain('twitter-tools-og-hook');
+
+function aktt_og_post_options() {
+	global $self, $post;
+
+	$self == 'post-new.php' ? $notify = get_option('aktt_og_default', 'yes'): $notify = get_post_meta($post->ID, '_aktt_notify_fb', true); 
+	$ogurl = get_option('aktt_og_url', '');
+	$facebook_app_id = get_option('aktt_og_app_id', '');
+	$facebook_secret = get_option('aktt_og_secret', '');
+
+	if($ogurl == '' || $facebook_app_id == '' || $facebook_secret == '')
+	{
+		return;
+	}
+
+	echo '<p>'.__('Send post to Facebook?', 'twitter-tools-og-hook').'
+                &nbsp;
+                <input type="radio" name="_aktt_notify_fb" id="_aktt_notify_fb_yes" value="yes" '.checked('yes', $notify, false).' /> <label for="aktt_notify_fb_yes">'.__('Yes', 'twitter-tools-og-hook').'</label> &nbsp;&nbsp;
+                <input type="radio" name="_aktt_notify_fb" id="_aktt_notify_fb_no" value="no" '.checked('no', $notify, false).' /> <label for="aktt_notify_fb_no">'.__('No', 'twitter-tools-og-hook').'</label>
+</p>';
+}
+add_action('aktt_post_options', 'aktt_og_post_options');
+
+function aktt_og_do_blog_post_og($post_id = 0) {
+
+        global $aktt;
+
+	$oged = get_post_meta($post_id, 'aktt_oged', true);
+	$notify = get_post_meta($post_id, '_aktt_notify_fb', true);
+	$ogurl = get_option('aktt_og_url', '');
+	$facebook_app_id = get_option('aktt_og_app_id', '');
+	$facebook_secret = get_option('aktt_og_secret', '');
+
+	if($ogurl == '' || $facebook_app_id == '' || $facebook_secret == '' || $post_id == 0 || $notify != 'yes' || $oged == '1')
+	{
+		return;
+	}
+
+	$post = get_post($post_id);
+	// check for an edited post before TT was installed
+	if ($post->post_date <= $aktt->install_date) {
+		return;
+	}
+	// check for private posts
+	if ($post->post_status == 'private') {
+		return;
+	}
+
+	$url = apply_filters('tweet_blog_post_url', get_permalink($post_id));
+	$og_text = sprintf(__($aktt->tweet_format, 'twitter-tools'), @html_entity_decode($post->post_title, ENT_COMPAT, 'UTF-8'), $url);
+
+	$mymessage = $og_text;
+
+	$access_token_url = "https://graph.facebook.com/oauth/access_token"; 
+	$parameters = "grant_type=client_credentials&client_id=" . $facebook_app_id .
+		"&client_secret=" . $facebook_secret;
+	$access_token = file_get_contents($access_token_url ."?".$parameters);
+
+	$apprequest_url = "https://graph.facebook.com/feed";
+	$parameters = "?" . $access_token . "&message=" .
+		urlencode($mymessage) . "&id=" . $ogurl . "&method=post";
+	$myurl = $apprequest_url . $parameters;
+        $result = file_get_contents($myurl);
+
+	add_post_meta($post_id, 'aktt_oged', '1', true); 
+}
+add_action('publish_post', 'aktt_og_do_blog_post_og', 99);
+
+function aktt_og_save_post($post_id, $post) {
+	if (current_user_can('edit_post', $post_id)) {
+		update_post_meta($post_id, '_aktt_notify_fb', $_POST['_aktt_notify_fb']);
+	}
+}
+add_action('save_post', 'aktt_og_save_post', 10, 2);
+
+function aktt_og_request_handler() {
+	if (!empty($_POST['cf_action'])) {
+		switch ($_POST['cf_action']) {
+			case 'aktt_og_update_settings':
+				if (!wp_verify_nonce($_POST['_wpnonce'], 'aktt_og_update_settings')) {
+					wp_die('Oops, please try again.');
+				}
+				aktt_og_save_settings();
+				wp_redirect(admin_url('options-general.php?page=twitter-tools.php&updated=true'));
+				die();
+				break;
+		}
+	}
+}
+add_action('init', 'aktt_og_request_handler');
+
+$aktt_og_settings = array(
+	'aktt_og_default' => array(
+		'type' => 'select',
+		'label' => __('Set this on by default?', 'twitter-tools-og-hook'),
+		'default' => 'yes',
+		'options' => array(
+			'yes' => 'Yes',
+			'no' => 'No',
+		),
+		'help' => __('Set this on by default?', 'twitter-tools-og-hook'),
+	),
+        'aktt_og_url' => array(
+                'type' => 'string',
+                'label' => __('OG URL', 'twitter-tools-og-hook'),
+                'default' => '',
+                'help' => __('The URL of the facebook OG', 'twitter-tools-og-hook'),
+        ),
+        'aktt_og_app_id' => array(
+                'type' => 'string',
+                'label' => __('OG App ID', 'twitter-tools-og-hook'),
+                'default' => '',
+                'help' => __('The App ID of the facebook OG', 'twitter-tools-og-hook'),
+        ),
+        'aktt_og_secret' => array(
+                'type' => 'string',
+                'label' => __('OG App Secret', 'twitter-tools-og-hook'),
+                'default' => '',
+                'help' => __('The secret of the facebook OG', 'twitter-tools-og-hook'),
+        ),
+
+
+);
+
+function aktt_og_setting($option) {
+	$value = get_option($option);
+	if (empty($value)) {
+		global $aktt_og_settings;
+		$value = $aktt_og_settings[$option]['default'];
+	}
+	return $value;
+}
+
+if (!function_exists('cf_settings_field')) {
+	function cf_settings_field($key, $config) {
+		$option = get_option($key);
+		if (empty($option) && !empty($config['default'])) {
+			$option = $config['default'];
+		}
+		$label = '<label for="'.$key.'">'.$config['label'].'</label>';
+		$help = '<span class="help">'.$config['help'].'</span>';
+		switch ($config['type']) {
+			case 'select':
+				$output = $label.'<select name="'.$key.'" id="'.$key.'">';
+				foreach ($config['options'] as $val => $display) {
+					$option == $val ? $sel = ' selected="selected"' : $sel = '';
+					$output .= '<option value="'.$val.'"'.$sel.'>'.htmlspecialchars($display).'</option>';
+				}
+				$output .= '</select>'.$help;
+				break;
+			case 'textarea':
+				$output = $label.'<textarea name="'.$key.'" id="'.$key.'">'.htmlspecialchars($option).'</textarea>'.$help;
+				break;
+			case 'string':
+			case 'int':
+			default:
+				$output = $label.'<input name="'.$key.'" id="'.$key.'" value="'.htmlspecialchars($option).'" />'.$help;
+				break;
+		}
+		return '<div class="option">'.$output.'<div class="clear"></div></div>';
+	}
+}
+
+function aktt_og_settings_form() {
+	global $aktt_og_settings;
+
+	print('
+<div class="wrap">
+	<h2>'.__('OG Hook for Twitter Tools', 'twitter-tools-og-hook').'</h2>
+	<form id="aktt_og_settings_form" name="aktt_og_settings_form" class="aktt" action="'.admin_url('options-general.php').'" method="post">
+		<input type="hidden" name="cf_action" value="aktt_og_update_settings" />
+		<fieldset class="options">
+	');
+	foreach ($aktt_og_settings as $key => $config) {
+		echo cf_settings_field($key, $config);
+	}
+	print('
+		</fieldset>
+		<p class="submit">
+			<input type="submit" name="submit" class="button-primary" value="'.__('Save Settings', 'twitter-tools-og-hook').'" />
+		</p>
+		'.wp_nonce_field('aktt_og_update_settings', '_wpnonce', true, false).wp_referer_field(false).'
+	</form>
+</div>
+	');
+}
+add_action('aktt_options_form', 'aktt_og_settings_form');
+
+function aktt_og_save_settings() {
+	if (!current_user_can('manage_options')) {
+		return;
+	}
+	global $aktt_og_settings;
+	foreach ($aktt_og_settings as $key => $option) {
+		$value = '';
+		switch ($option['type']) {
+			case 'int':
+				$value = intval($_POST[$key]);
+				break;
+			case 'select':
+				$test = stripslashes($_POST[$key]);
+				if (isset($option['options'][$test])) {
+					$value = $test;
+				}
+				break;
+			case 'string':
+			case 'textarea':
+			default:
+				$value = stripslashes($_POST[$key]);
+				break;
+		}
+		update_option($key, $value);
+	}
+}
+
+?>
